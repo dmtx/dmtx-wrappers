7bc963af7e769796a7e435ba3f0f2747770f9bd4
Vadim A. Misbakh-Soloviov <mva@mva.name>
2013-12-09 17:04:40 +0700

    fix ruby>1.8 incompatibility due to RString
    Signed-off-by: Vadim A. Misbakh-Soloviov <mva@mva.name>

--- ruby/Rdmtx.c
+++ ruby/Rdmtx.c
@@ -35,7 +35,7 @@ static VALUE rdmtx_decode(VALUE self, VALUE image /* Image from RMagick (Magick:
 
     VALUE safeImageString = StringValue(rawImageString);
 
-    char * imageBuffer = RSTRING(safeImageString)->ptr;
+    char * imageBuffer = RSTRING_PTR(safeImageString);
 
     int width = NUM2INT(rb_funcall(image, rb_intern("columns"), 0));
     int height = NUM2INT(rb_funcall(image, rb_intern("rows"), 0));
@@ -90,8 +90,8 @@ static VALUE rdmtx_encode(VALUE self, VALUE string) {
     dmtxEncodeSetProp(enc, DmtxPropSizeRequest, DmtxSymbolSquareAuto);
 
     /* Create barcode image */
-    if (dmtxEncodeDataMatrix(enc, RSTRING(safeString)->len,
-            (unsigned char *)RSTRING(safeString)->ptr) == DmtxFail) {
+    if (dmtxEncodeDataMatrix(enc, RSTRING_LEN(safeString),
+            (unsigned char *)RSTRING_PTR(safeString)) == DmtxFail) {
 //        printf("Fatal error !\n");
         dmtxEncodeDestroy(&enc);
         return Qnil;
